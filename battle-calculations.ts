c83ba33e8f7d201e4c6791fb583e9ab6c713df28
Stuhl <BasicsDone7@web.de>
2020-02-17 05:22:50 +0100

    added calculation file for battle related stuff

---
+++ battle-calculations.ts
@@ -0,0 +1,41 @@
+import weapons from "./weapons"
+
+interface Player {
+  weapon: string,
+  strength: number
+}
+
+interface Enemy {
+  defense: number
+}
+
+const utility = {
+  getWeapon(weapon: string) {
+    for (let arrayWeapon of weapons) {
+      if (arrayWeapon.name === weapon) {
+        return arrayWeapon
+      }
+    }
+  },
+
+  variance(value): number {
+    const dividedVal: number = value / 8
+    return Math.floor((Math.random() * dividedVal * 2) - dividedVal)
+  }
+}
+
+const battleCalculations = {
+  calculateDamage(player: Player, enemy: Enemy): number {
+    const weapon = utility.getWeapon(player.weapon)
+    let totalStrength: number = weapon.power + player.strength
+    const randomVal: number = utility.variance(weapon.power)
+
+    if (weapon.isMagical) {
+      totalStrength *= weapon.magicalPower
+    }
+
+    return ((totalStrength - (enemy.defense / 2)) / 2) + randomVal
+  }
+}
+
+export default battleCalculations
