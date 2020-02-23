af5d98e679e06011b966965d39bddfa9e214a106
Stuhl <BasicsDone7@web.de>
2020-02-23 19:43:21 +0100

    first prototype

--- state-machine.ts
+++ state-machine.ts
@@ -1,61 +1,51 @@
-const stateObj = {
-  initial: "booting",
-  states: [
-    {
-      name: "menu",
-      from: "booting",
-      to: "menu"
-    },
-    {
-      name: "play",
-      from: "menu",
-      to: "playing"
-    }
-  ],
-  methods: {
-    onMenu() {
-      console.log("menu transition")
-    },
-    onPlay() {
-      console.log("playing transition")
-    }
-  }
+interface state {
+  name: string
+  from: string
+  to: string
+}
+
+interface stateObject {
+  initial: string
+  states: state[]
+  methods: object
 }
 
 class StateMachine {
   state: string
   private methods: object
 
-  constructor(stateObj) {
+  constructor(stateObj: stateObject) {
     const {initial, states, methods} = stateObj
     this.state = initial
     this.methods = methods
+    this.generateStates(states)
   }
 
-  private generateStates(states) {
+  private generateStates(states: state[]) {
     for (let state of states) {
       this.generateFunction(state)
     }
   }
 
-  private formatName(name): string {
+  private formatName(name: string): string {
     const firstLetter = name[0]
     const rest = name.slice(1)
 
     return "on" + firstLetter.toUpperCase() + rest
   }
 
-  private getMethod(name) {
+  private getMethod(name: string) {
     return this.methods[this.formatName(name)]
   }
 
-  private generateFunction(state) {
+  private generateFunction(state: state) {
     const {name, from, to} = state
 
     this[name] = (): void => {
       if (this.state === from) {
         this.state = to
-
+        const method = this.getMethod(name)
+        method()
       }
     }
   }
