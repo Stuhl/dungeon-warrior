d1c4bfcdee9aaf71675160eaf95d517bd0bc1cef
Stuhl <BasicsDone7@web.de>
2020-02-23 19:37:43 +0100

    added formatName, getMethod func

--- state-machine.ts
+++ state-machine.ts
@@ -23,16 +23,40 @@ const stateObj = {
 }
 
 class StateMachine {
+  state: string
+  private methods: object
+
   constructor(stateObj) {
     const {initial, states, methods} = stateObj
     this.state = initial
+    this.methods = methods
   }
 
   private generateStates(states) {
     for (let state of states) {
-      const {name, from, to} = state
-      
+      this.generateFunction(state)
+    }
+  }
 
+  private formatName(name): string {
+    const firstLetter = name[0]
+    const rest = name.slice(1)
+
+    return "on" + firstLetter.toUpperCase() + rest
+  }
+
+  private getMethod(name) {
+    return this.methods[this.formatName(name)]
+  }
+
+  private generateFunction(state) {
+    const {name, from, to} = state
+
+    this[name] = (): void => {
+      if (this.state === from) {
+        this.state = to
+
+      }
     }
   }
 }
