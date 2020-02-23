2bb1574a83402f6ec07690edfcaced7e6dd82933
Stuhl <BasicsDone7@web.de>
2020-02-23 19:07:54 +0100

    testing game loop

--- index.ts
+++ index.ts
@@ -1,18 +1,21 @@
 import weapons from "./weapons"
 import battleCalculations from "./battle-calculations"
+import loop from "./loop"
 
-console.log(battleCalculations)
+requestAnimationFrame(loop)
 
-const {calculateDamage} = battleCalculations
+// console.log(battleCalculations)
 
-const player = {
-  weapon: "Zweihander",
-  strength: 20
-}
-
-const enemy = {
-  defense: 5
-}
-
-const damage = calculateDamage(player, enemy)
-console.log(damage)
+// const {calculateDamage} = battleCalculations
+//
+// const player = {
+//   weapon: "Zweihander",
+//   strength: 20
+// }
+//
+// const enemy = {
+//   defense: 5
+// }
+//
+// const damage = calculateDamage(player, enemy)
+// console.log(damage)
