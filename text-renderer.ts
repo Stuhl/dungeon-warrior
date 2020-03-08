aa2a0db01c88dd81600be0967f4faf9ce2edea41
Stuhl <BasicsDone7@web.de>
2020-03-08 13:12:31 +0100

    basic text renderer

---
+++ text-renderer.ts
@@ -0,0 +1,32 @@
+class TextRenderer {
+  text: string
+
+  constructor() {
+    this.text = ""
+  }
+
+  private remove(): void {
+    this.text = ""
+  }
+
+  private prepare(text) {
+    const paragraph = document.createElement("p")
+    const textNode = document.createTextNode(text)
+
+    paragraph.style.fontSize = "5em"
+    paragraph.appendChild(textNode)
+    return paragraph
+  }
+
+  add(text): void {
+    this.text = text
+  }
+
+  render(): void {
+    const node = this.prepare(this.text)
+    this.remove()
+    document.body.appendChild(node)
+  }
+}
+
+export default TextRenderer
